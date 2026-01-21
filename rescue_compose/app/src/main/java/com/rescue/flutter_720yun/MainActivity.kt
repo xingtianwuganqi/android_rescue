e727e4cba4eafdf0f750e3ece6b7d439ab352c6d
jun jing <rxswift@126.com>
2026-01-21 22:18:36 +0800

    fix

---
+++ rescue_compose/app/src/main/java/com/rescue/flutter_720yun/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.rescue.flutter_720yun
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
+import com.rescue.flutter_720yun.demo.DemoActivity
+import com.rescue.flutter_720yun.ui.theme.RescuecomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RescuecomposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Spacer(modifier = Modifier.padding(16.dp))
+    Button(
+        onClick = {
+
+        }
+    ) {
+        Text(text = "点击")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RescuecomposeTheme {
+        Greeting("Android")
+    }
+}
